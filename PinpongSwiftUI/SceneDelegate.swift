c03e290f934bd6fdc39b26aaa76eecdcd1f7ed31
Aleksandr Anosov <major.pein@gmail.com>
2019-07-04 18:01:49 +0300

    тайтлы

--- PinpongSwiftUI/SceneDelegate.swift
+++ PinpongSwiftUI/SceneDelegate.swift
@@ -17,19 +17,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            let ratingsController = UINavigationController(rootViewController: UIHostingController(rootView: RatingsView()))
-            ratingsController.navigationBar.prefersLargeTitles = true
+
+            let ratingsController = UIHostingController(rootView: RatingsView())
+            let ratingsNavigationController = UINavigationController(rootViewController: ratingsController)
+            ratingsNavigationController.navigationBar.prefersLargeTitles = true
             ratingsController.title = "Рейтинг"
-            let gamesController = UINavigationController(rootViewController: UIHostingController(rootView: GamesView()))
-            gamesController.navigationBar.prefersLargeTitles = true
+
+            let gamesController = UIHostingController(rootView: GamesView())
+            let gamesNavigationController = UINavigationController(rootViewController: gamesController)
+            gamesNavigationController.navigationBar.prefersLargeTitles = true
             gamesController.title = "Игры"
-            let profileController = UINavigationController(rootViewController: UIHostingController(rootView: ProfileView(isMe: true)))
-            profileController.navigationBar.prefersLargeTitles = true
+
+            let profileController = UIHostingController(rootView: ProfileView(isMe: true))
+            let profileNavigationController = UINavigationController(rootViewController: profileController)
+            profileNavigationController.navigationBar.prefersLargeTitles = true
             profileController.title = "Профиль"
+
             let tabbarController = UITabBarController()
             
             tabbarController.setViewControllers(
-                [ratingsController, gamesController, profileController],
+                [ratingsNavigationController, gamesNavigationController, profileNavigationController],
                 animated: false
             )
             
