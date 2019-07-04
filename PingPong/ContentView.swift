5eaca763e12a301e73cb5dff1dfd530cbae55767
Мирский Степан Алексеевич <samirskiy@domclick.ru>
2019-07-04 09:37:30 +0300

    Initial commit

---
+++ PingPong/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PingPong
+//
+//  Created by Мирский Степан Алексеевич on 04/07/2019.
+//  Copyright © 2019 DomclickPingPongTeam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
