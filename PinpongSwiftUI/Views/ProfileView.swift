036f61f955ff85e35c210df484e1951adbe1f34b
Sergey Kochetov <sergey.bmstu@gmail.com>
2019-07-04 12:56:35 +0300

    Profile

--- PinpongSwiftUI/Views/ProfileView.swift
+++ PinpongSwiftUI/Views/ProfileView.swift
@@ -8,8 +8,29 @@
 
 import SwiftUI
 
+struct User {
+    let name: String
+    let image: URL?
+}
+
 struct ProfileView : View {
+    var user: User = User.init(name: "Sergey", image: nil)
+    
     var body: some View {
-        Text("Profile")
+        NavigationView {
+            ScrollView {
+                VStack.init(alignment: .center, spacing: 0) {
+                    Image("defaultImage")
+                        .frame(width: 300, height: 300)
+                        .aspectRatio(contentMode: .fill)
+                        
+                        .clipShape(Circle())
+                    Text(user.name)
+                        .fontWeight(.heavy)
+                        .font(Font.system(size: 30, design: .rounded))
+                }
+            }
+            .navigationBarTitle("Профиль")
+        }
     }
 }
