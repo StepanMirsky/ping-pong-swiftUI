836090627174a1ef79b1c56f2a4df54dbd18b7cf
Aleksandr Anosov <major.pein@gmail.com>
2019-07-05 01:07:33 +0300

    Теперь можно создавать игры!

--- PinpongSwiftUI/Views/ProfileView.swift
+++ PinpongSwiftUI/Views/ProfileView.swift
@@ -9,6 +9,8 @@
 import SwiftUI
 
 struct ProfileView : View {
+    let userDefaults = UserDefaults.standard
+
     var user: User = User(
         name: "Sergey",
         rating: 1000,
@@ -21,51 +23,50 @@ struct ProfileView : View {
 
     var isMe: Bool
 
-    var homeUser: User {
-        return User(
-            name: "Это я",
-            rating: 1000,
-            image: UIImage(named: "defaultImage")!,
-            lastGames: [
-                ShortGame(isWin: false),
-                ShortGame(isWin: true)
-            ]
-        )
+    var isAuthorized: Bool {
+        return userDefaults.string(forKey: "token") != nil
     }
 
     var body: some View {
         ScrollView {
             
             VStack(alignment: .center, spacing: 20) {
-                if isMe {
-                    PresentationLink(destination: RegistrationView()) {
+                if isAuthorized || !isMe {
+                    if isMe {
+                        PresentationLink(destination: RegistrationView()) {
+                            Image(uiImage: user.image)
+                                .frame(width: 300, height: 300)
+                                .aspectRatio(contentMode: .fill)
+                                .clipShape(Circle())
+                        }
+                    } else {
                         Image(uiImage: user.image)
                             .frame(width: 300, height: 300)
                             .aspectRatio(contentMode: .fill)
                             .clipShape(Circle())
                     }
-                } else {
-                    Image(uiImage: user.image)
-                        .frame(width: 300, height: 300)
-                        .aspectRatio(contentMode: .fill)
-                        .clipShape(Circle())
-                }
-                Text(user.name)
-                    .font(Font.system(.largeTitle, design: .rounded))
-                Text("\(user.rating)")
-                    .color(Color.ratingColor(user.rating))
-                    .font(Font.system(.headline, design: .rounded))
-                LastGamesView(lastGames: user.lastGames)
-                if !isMe {
-                    //TODO: как вызвать метод gamesService.create()?
-                    NavigationLink(destination: GameView(game: Game(homeUser: homeUser, awayUser: user, homeScore: 0, awayScore: 0, isFinished: false))) {
-                        Text("Вызвать")
+                    Text(user.name)
+                        .font(Font.system(.largeTitle, design: .rounded))
+                    Text("\(user.rating)")
+                        .color(Color.ratingColor(user.rating))
+                        .font(Font.system(.headline, design: .rounded))
+                    LastGamesView(lastGames: user.lastGames)
+                    if !isMe {
+                        NavigationLink(destination: GameView(game: nil, awayUser: self.user)) {
+                            Text("Вызвать")
+                                .font(.system(.headline, design: .rounded))
+                        }
+                    }
+                    Spacer()
+                    NavigationLink(destination: GamesView(userName: user.name)) {
+                        Text("Посмотреть все игры")
                             .font(.system(.headline, design: .rounded))
                     }
-                }
-                Spacer()
-                NavigationLink(destination: GamesView(userName: user.name)) {
-                    Text("Посмотреть все игры")
+                } else {
+                    PresentationLink(destination: LoginView()) {
+                        Text("Войти или зарегистрироваться")
+                            .font(.system(.headline, design: .rounded))
+                    }
                 }
             }
         }
